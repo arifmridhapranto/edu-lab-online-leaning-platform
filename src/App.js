715739d82aebd748b7ef0becaeba49c2541972d0
arifmridhapranto <mridhapranto647@gmail.com>
2021-10-04 02:27:41 +0600

    routing done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import "./App.css";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Blogs from "./components/Blogs/Blogs";
+import About from "./components/About/About";
+import Services from "./components/Services/Services";
+import Contact from "./components/Contact/Contact";
+import NotFound from "./components/NotFound/NotFound";
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/home'>
+            <Home />
+          </Route>
+          <Route path='/blogs'>
+            <Blogs />
+          </Route>
+          <Route path='/about'>
+            <About />
+          </Route>
+          <Route path='/services'>
+            <Services />
+          </Route>
+          <Route path='/contact'>
+            <Contact />
+          </Route>
+          <Route path='*'>
+            <NotFound />
+          </Route>
+        </Switch>
+      </BrowserRouter>
       <Footer />
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
 import logo from "../../edulab.jpg";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <div>
-      <div className='logo'>
-        <img src={logo} alt='' style={{ height: "150", width: "150" }} />
+    <>
+      <div>
+        <NavLink to='/'>
+          <img src={logo} alt='' className='logo' />
+        </NavLink>
+        <div className='me-auto'>
+          <NavLink to='/home'>Home</NavLink>
+          <NavLink to='/about'>About</NavLink>
+          <NavLink to='/services'>Services</NavLink>
+          <NavLink to='/contact'>Contact</NavLink>
+          <NavLink to='/blogs'>Blogs</NavLink>
+        </div>
       </div>
-      <div className='manu'></div>
-    </div>
+    </>
   );
 };
 

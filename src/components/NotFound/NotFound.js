8eb537bbeee23d549dfc03c7fb31f03f8cc3350e
arifmridhapranto <mridhapranto647@gmail.com>
2021-10-04 23:28:25 +0600

    404 page updated

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,21 +1,24 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+import errorpage from "../../img/404error.jpg";
+import Button from "react-bootstrap/Button";
 
 const NotFound = () => {
+  let history = useHistory();
+  const backToHome = () => {
+    history.push("/");
+  };
   return (
     <>
-      <div id='notfound'>
-        <div className='notfound'>
-          <div className='notfound-404'>
-            <h1>404</h1>
-          </div>
-          <h2>WE ARE SORRY, PAGE NOT FOUND!</h2>
-          <p>
-            THE PAGE YOU ARE LOOKING FOR MIGHT HAVE BEEN REMOVED HAD ITS NAME
-            CHANGED OR IS TEMPORARILY UNAVAILABLE.
-          </p>
-          <NavLink to='/'>back to homepage</NavLink>
-        </div>
+      <div className='container text-center'>
+        <img src={errorpage} alt='' className='img-fluid' />
+        <Button
+          variant='primary'
+          className='mt-5'
+          size='lg'
+          onClick={backToHome}>
+          Back To Home page
+        </Button>
       </div>
     </>
   );
